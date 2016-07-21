6d1d03670dc33157ae57c59f4343787f05d3359c
TED <tedlade.tl@gmail.com>
2016-07-21 20:35:09 +0100

    Testing

--- dev/modules/auth/auth.js
+++ dev/modules/auth/auth.js
@@ -12,18 +12,20 @@ angular.module('app.auth', ['ui.router'])
                 url: '/auth-return',
                 templateUrl: 'modules/auth/index.html',
                 controller: 'SlackCtrl',
-                // resolve: {
-                //     leaderboard: ['API', function(API) {
-                //         return API.all('leaderboard').getList();
-                //     }]
-                // }
+                resolve: {
+                    User: ['API', function(API) {
+                        return API.all('leaderboard').getList();
+                    }]
+                }
             })
     }
 ])
 
 .controller('SlackCtrl', ['$scope', '$rootScope', '$state', 'Notification', 'LocalService', 'Auth', '$stateParams',
     function($scope, $rootScope, $state, Notification, LocalService, Auth, $stateParams) {
-        console.log($stateParams)
+
+    	var token = $stateParams.auth_token;
+        console.log(token)
         
         $scope.userId = $stateParams.client_id;
         $scope.token = $stateParams.auth_token;
